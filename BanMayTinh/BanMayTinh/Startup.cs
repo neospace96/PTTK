fbd74dc6c8ea979b4588437ad11a137fcf526d70
Dinh Nguyen <nguyendinhteki@gmail.com>
2017-10-11 00:52:20 +0700

    Init Project

---
+++ BanMayTinh/BanMayTinh/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BanMayTinh.Startup))]
+namespace BanMayTinh
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
